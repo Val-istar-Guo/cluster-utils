c384f65a0dca58c0f270e9dae65fc6932da3c475
val.istar.guo <val.istar.guo@gmail.com>
2023-03-17 23:20:43 +0800

    feat: auto clean flannel

--- bin/cluster/reset.sh
+++ bin/cluster/reset.sh
@@ -3,3 +3,13 @@ set -e
 
 # kubeadm reset -f --cri-socket=unix:///var/run/cri-dockerd.sock
 kubeadm reset -f
+
+systemctl stop kubelet
+rm -rf /var/lib/cni/*
+rm -rf /var/lib/kubelet/*
+rm -rf /etc/cni/*
+ifconfig cni0 down
+ifconfig flannel.1 down
+ip link delete cni0
+ip link delete flannel.1
+systemctl start kubelet

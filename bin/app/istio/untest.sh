3a3cd209d9589ac7bf1ab800c650f20c20124ae7
val.istar.guo <val.istar.guo@gmail.com>
2024-02-13 01:03:23 +0800

    feat: split istio test yaml

--- bin/app/istio/untest.sh
+++ bin/app/istio/untest.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-kubectl delete -f ./manifests/test.yaml
+kubectl delete \
+  -f ./manifests/test.namespace.yaml \
+  -f ./manifests/test.deployment.yaml \
+  -f ./manifests/test.service.yaml \
+  -f ./manifests/test.gateway.yaml \
+  -f ./manifests/test.virtual-service.yaml
